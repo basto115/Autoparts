cda881baf31b75d623af575cc22721d8658599e9
jotakaii <jose.duran@duocuc.cl>
2025-05-23 10:59:03 -0400

    drf

--- autoparts/settings.py
+++ autoparts/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tienda',              # Tu app creada
+    'rest_framework',      # Django REST Framework
 ]
 
 MIDDLEWARE = [
